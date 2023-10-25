f1d18131511894fdccdfa7eae15d0371268b273d
Art Wu <50754138+artqwu@users.noreply.github.com>
2023-10-25 11:32:10 -0400

    added nonfunctional date pickers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+'use client'
+
 // import Image from 'next/image'
 import TextField from '@mui/material/TextField';
+import { DatePicker } from '@mui/x-date-pickers';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 export default function Home() {
   const inputProps = {
@@ -12,7 +17,11 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Activisor</h1>
       <div id="container" className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <TextField id="schedule-name" type="text" inputProps={inputProps}/>
+        <TextField id="schedule-name" type="text" inputProps={inputProps} />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker label="Start on" />
+          <DatePicker label="End by" />
+        </LocalizationProvider>
       </div>
     </main>
   )
