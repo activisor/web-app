1a3572e63da6af52f2f4a4e7b4e101ad00e39632
Art Wu <50754138+artqwu@users.noreply.github.com>
2023-11-02 22:46:05 -0400

    added Jest test framework

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,4 +1,5 @@
 // `app/schedule.tsx` is the UI for the `/schedule` URL
+
 export default function Schedule() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
