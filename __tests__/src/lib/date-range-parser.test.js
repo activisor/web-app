be3d610a34dd48da699d3dda18ea7ade40cb21cd
Art Wu <50754138+artqwu@users.noreply.github.com>
2023-11-15 21:33:24 -0500

    DateRangeParser parses <1 week date range with 1 week frequency

--- __tests__/src/lib/date-range-parser.test.js
+++ __tests__/src/lib/date-range-parser.test.js
@@ -1,7 +1,20 @@
 import { DateRangeParser } from '@/lib/sheets/date-range-parser';
 import Frequency from '@/lib/frequency';
 
-test('parses 1+ week date range with 1 week frequency', () => {
+test('parses <1 week date range with 1 week frequency', () => {
+  const sut = new DateRangeParser();
+
+  const startDate = new Date(2021, 0, 1);
+  const endDate = new Date(2021, 0, 7);
+  const result = sut.parse(startDate, endDate, Frequency.Weekly);
+
+  expect(result.length).toBe(1);
+  expect(result[0].getFullYear()).toBe(2021);
+  expect(result[0].getMonth()).toBe(0);
+  expect(result[0].getDate()).toBe(1);
+});
+
+test('parses >1 week date range with 1 week frequency', () => {
   const sut = new DateRangeParser();
 
   const startDate = new Date(2021, 0, 1);
