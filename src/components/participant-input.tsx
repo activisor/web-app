190adf1d6c4cfec05c310dc06658c036b87ef788
Art Wu <50754138+artqwu@users.noreply.github.com>
2023-11-20 14:01:55 -0500

    autosubmit participant on 'Enter'

--- src/components/participant-input.tsx
+++ src/components/participant-input.tsx
@@ -30,14 +30,31 @@ const ParticipantInput: React.FC<ParticipantInputProps> = (props) => {
         }
     };
 
+    const handleEmailKeyDown = (event: React.KeyboardEvent<HTMLInputElement>): void => {
+        if (event.key === 'Enter' && props_.saved) {
+            publish(CHANGE_EVENT, props_);
+        }
+    }
+
     const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
         let tempProps = { ...props_ };
         tempProps.name = event.target.value;
         setProps_(tempProps);
-        if (props_.saved) {
-            publish(CHANGE_EVENT, tempProps);
-        }
     };
+
+    const handleNameKeyDown = (event: React.KeyboardEvent<HTMLInputElement>): void => {
+        if (event.key === 'Enter' && props_.saved) {
+            publish(CHANGE_EVENT, props_);
+        } else if (event.key === 'Enter' && props_.email && props_.name) {
+            publish(ADD_EVENT, props_);
+            const initialProps: ParticipantInputProps = {
+                name: '',
+                email: '',
+                saved: false
+            };
+            setProps_(initialProps);
+        }
+    }
 
     const handleAddClick = (event: React.MouseEvent<HTMLButtonElement>): void => {
         publish(ADD_EVENT, props_);
@@ -60,12 +77,14 @@ const ParticipantInput: React.FC<ParticipantInputProps> = (props) => {
                 type="text"
                 inputProps={{ placeholder: 'Email', value: props_.email }}
                 onChange={handleEmailChange}
+                onKeyDown={handleEmailKeyDown}
                 css={{ marginRight: 8 }} />
             <TextField
                 id="participant-name"
                 type="text"
                 inputProps={{ placeholder: 'Name', value: props_.name }}
-                onChange={handleNameChange} />
+                onChange={handleNameChange}
+                onKeyDown={handleNameKeyDown} />
             {props.saved ? (
                 <IconButton aria-label="delete" color="primary" onClick={handleDeleteClick}>
                     <DeleteIcon />
