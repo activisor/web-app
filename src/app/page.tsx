19d6e151e3fd22d5b72bd6d31598bbf76841e86f
Art Wu <50754138+artqwu@users.noreply.github.com>
2023-11-28 12:33:16 -0500

    added 'building page animation'
    https://trello.com/c/Aqn0lZSH

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,54 @@
 
 import { css } from '@emotion/react'
 import { useSession } from 'next-auth/react';
+import Skeleton from '@mui/material/Skeleton';
 import ScheduleInput from '@/components/schedule-input';
 import { readItem, saveItem, GENERATION_REQUESTED, SCHEDULE_DATA } from '@/client-lib/local-storage';
 import type { ScheduleData } from '@/lib/schedule-data';
 
 export default function Home() {
     const { data: session, status } = useSession();
+    const generationRequested: boolean = readItem(GENERATION_REQUESTED) as boolean;
+    const testFlag: boolean = generationRequested && true;
+    const sampleData = 'test_data';
+
+    const scrimCss = generationRequested ? css({
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        minHeight: '100vh',
+        minWidth: '100vw',
+        backdropFilter: 'blur(2px)',
+        zIndex: 100,
+    }) : css({
+        display: 'none',
+    });
+
+    const buildSkeletonRowCss = css({
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginBottom: 4,
+        '@media(min-width: 1248px)': {
+            marginBottom: 8
+        }
+    });
+
+    const buildSkeletonCell1Css = css({
+        width: '10%',
+        marginRight: 4,
+        '@media(min-width: 1248px)': {
+            marginRight: 4,
+        }
+    });
+
+    const buildSkeletonCell2Css = css({
+        width: '90%'
+    });
 
     // if token detected, get DTO, compress & redirect to /schedule with payload
     if (status === "authenticated") {
-        const generationRequested = readItem(GENERATION_REQUESTED);
         if (!generationRequested) {
             saveItem(GENERATION_REQUESTED, true);
             const dto: ScheduleData = readItem(SCHEDULE_DATA);
@@ -38,12 +76,50 @@ export default function Home() {
         }
 
         return (
-            <main>
-                <h1 css={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}>Building Your Schedule</h1>
+            <main css={{
+                display: 'flex',
+                justifyContent: 'center',
+                width: '100%',
+                /* breakpoint for large screen overrides, 1280px wide */
+                '@media(min-width: 1248px)': {
+                    marginTop: 16
+                }
+            }}>
+                <div css={{
+                        width: '100%',
+                        height: '100vh',
+                        '@media(min-width: 1248px)': {
+                            width: '50%'
+                        }
+                    }}>
+                    <h1 css={{
+                        textAlign: 'center',
+                        marginBottom: '10vh',
+                        '@media(max-width: 1247px)': {
+                            marginTop: 8
+                        }
+                    }}>Building Your Schedule</h1>
+                    <div css={buildSkeletonRowCss}>
+                        <Skeleton variant="rectangular" height={'5vh'} css={buildSkeletonCell1Css} />
+                        <Skeleton variant="rectangular" height={'5vh'} css={buildSkeletonCell2Css} />
+                    </div>
+                    <div css={buildSkeletonRowCss}>
+                        <Skeleton variant="rectangular" height={'5vh'} css={buildSkeletonCell1Css} />
+                        <Skeleton variant="rectangular" height={'5vh'} css={buildSkeletonCell2Css} />
+                    </div>
+                    <div css={buildSkeletonRowCss}>
+                        <Skeleton variant="rectangular" height={'5vh'} css={buildSkeletonCell1Css} />
+                        <Skeleton variant="rectangular" height={'5vh'} css={buildSkeletonCell2Css} />
+                    </div>
+                    <div css={buildSkeletonRowCss}>
+                        <Skeleton variant="rectangular" height={'5vh'} css={buildSkeletonCell1Css} />
+                        <Skeleton variant="rectangular" height={'5vh'} css={buildSkeletonCell2Css} />
+                    </div>
+                    <div css={buildSkeletonRowCss}>
+                        <Skeleton variant="rectangular" height={'5vh'} css={buildSkeletonCell1Css} />
+                        <Skeleton variant="rectangular" height={'5vh'} css={buildSkeletonCell2Css} />
+                    </div>
+                </div>
             </main>
         );
     }
@@ -52,6 +128,8 @@ export default function Home() {
         <main css={{
             display: 'flex',
             justifyContent: 'center',
+            minHeight: '100vh',
+            minWidth: '100vw',
             /* breakpoint for large screen overrides, 1280px wide */
             '@media(min-width: 1248px)': {
                 marginTop: 16
@@ -68,6 +146,7 @@ export default function Home() {
                 }}>Activisor</h1>
                 <ScheduleInput />
             </div>
+            <div id="scrim" css={scrimCss}></div>
         </main>
     );
 }
