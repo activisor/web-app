5e0b81a04f24f1e626aa9f34f1244f7405480f6c
Art Wu <50754138+artqwu@users.noreply.github.com>
2023-12-04 11:21:16 -0500

    integrated SendGridEmailExtractor into API endpoint

--- src/app/api/mail-relay/route.ts
+++ src/app/api/mail-relay/route.ts
@@ -1,6 +1,7 @@
 import type { NextRequest } from "next/server"
 import { appContainer } from '@/inversify.config';
 import { TYPES } from "@/inversify-types";
+import { EmailExtraction } from '@/lib/email/email-extraction';
 
 export async function POST(request: NextRequest) {
     // parse multipart/form-data
@@ -12,5 +13,10 @@ export async function POST(request: NextRequest) {
     console.log(`dkim: ${formData.get('dkim')}, ${typeof formData.get('dkim')}`);
     console.log(`from: ${formData.get('from')}`);
     console.log(`text: ${formData.get('text')}`);
+
+    const sendGridEmailExtractor = appContainer.get<EmailExtraction>(TYPES.SendGridEmailExtractor);
+    const email = sendGridEmailExtractor.extract(formData);
+    console.log(JSON.stringify(email));
+
     return new Response('', { status: 200 });
 }
