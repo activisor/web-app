22c0a76ecdc9ae16a9dd0d447a5f6f2bf92708f0
Art Wu <50754138+artqwu@users.noreply.github.com>
2023-12-05 22:41:54 -0500

    SCHEDULER_ENTRY_EMAIL consistency fix

--- src/inversify-types.ts
+++ src/inversify-types.ts
@@ -10,7 +10,7 @@ const TYPES = {
     SendGridEmailExtractor: Symbol.for('SendGridEmailExtractor'),
     SendGridEmailResponder: Symbol.for('SendGridEmailResponder'),
     DEVARIANCE_COEF: Symbol.for('DEVARIANCE_COEF'),
-    SCHEDULE_ENTRY_EMAIL: Symbol.for('SCHEDULE_ENTRY_EMAIL'),
+    SCHEDULER_ENTRY_EMAIL: Symbol.for('SCHEDULER_ENTRY_EMAIL'),
     SCHEDULER_RESPONDER_EMAIL: Symbol.for('SCHEDULER_RESPONDER_EMAIL'),
     SENDGRID_API_KEY: Symbol.for('SENDGRID_API_KEY'),
     SENDGRID_TEMPLATE_ID: Symbol.for('SENDGRID_TEMPLATE_ID'),
