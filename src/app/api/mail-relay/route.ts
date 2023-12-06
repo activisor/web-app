a70783793a982fe7d424861d299dbd3ffba1ef0e
Art Wu <50754138+artqwu@users.noreply.github.com>
2023-12-06 10:13:18 -0500

    removed dev logging

--- src/app/api/mail-relay/route.ts
+++ src/app/api/mail-relay/route.ts
@@ -14,16 +14,10 @@ export async function POST(request: NextRequest) {
     console.log(`from: ${formData.get('from')}`);
     console.log(`text: ${formData.get('text')}`);
 
-    console.log(`dkim: ${formData.get('dkim')}, ${typeof formData.get('dkim')}`);
-    console.log(`SPF: ${formData.get('SPF')}, ${typeof formData.get('SPF')}`);
-    console.log(`spam_score: ${formData.get('spam_score')}, ${typeof formData.get('spam_score')}`);
-    console.log(`spam_report: ${formData.get('spam_report')}`);
-
     const sendGridEmailSpamValidator = appContainer.get<FormDataValidation>(TYPES.SpamValidation);
     if (sendGridEmailSpamValidator.validate(formData)) {
         const sendGridEmailExtractor = appContainer.get<EmailExtraction>(TYPES.EmailExtraction);
         const email = sendGridEmailExtractor.extract(formData);
-        console.log(JSON.stringify(email));
 
         const sendGridEmailResponder = appContainer.get<EmailExtractProcessing>(TYPES.EmailExtractProcessing);
         const result = await sendGridEmailResponder.process(email);
