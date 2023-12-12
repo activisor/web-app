6c310fb761a25a10e7e74939868a0f674bb283b3
Art Wu <50754138+artqwu@users.noreply.github.com>
2023-12-12 13:28:25 -0500

    added Activisor theme

--- src/client-lib/provider.tsx
+++ src/client-lib/provider.tsx
@@ -5,14 +5,30 @@ import { SessionProvider } from "next-auth/react";
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#2196F3',     // blue
+        },
+        secondary: {
+            main: '#FF9800',   // orange
+        }
+    },
+});
+
+console.log(JSON.stringify(theme.palette));
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
-        <SessionProvider>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <CssBaseline />
-                {children}
-            </LocalizationProvider>
-        </SessionProvider>
+        <ThemeProvider theme={theme}>
+            <SessionProvider>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <CssBaseline />
+                    {children}
+                </LocalizationProvider>
+            </SessionProvider>
+        </ThemeProvider>
     );
 }
