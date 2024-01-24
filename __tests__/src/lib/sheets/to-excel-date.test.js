b6b4076ab91abf7f1d34c8f70974b501d0531457
Art Wu <50754138+artqwu@users.noreply.github.com>
2024-01-24 18:37:13 -0500

    bug fix: account for timezone

--- __tests__/src/lib/sheets/to-excel-date.test.js
+++ __tests__/src/lib/sheets/to-excel-date.test.js
@@ -6,6 +6,6 @@
 import { toExcelDate } from '@/lib/sheets/to-excel-date';
 
 test('converts Date to Excel serial date', () => {
-  const date = new Date('2021-01-01T00:00:00.000Z');
+  const date = new Date('2021-01-01T05:00:00.000Z');
   expect(toExcelDate(date)).toBe(44197);
 });
--- src/lib/sheets/to-excel-date.ts
+++ src/lib/sheets/to-excel-date.ts
@@ -6,7 +6,16 @@ const MS_PER_DAY = 86400000;
 // offset from 1/1/1970 to 1/1/1900 (Excel's epoch)
 const EPOCH_OFFSET_DAYS = 25569;
 
+function removeTime(date = new Date()) {
+    return new Date(
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDate()
+    );
+  }
+
 function toExcelDate(date: Date): number {
+    date = removeTime(date);
     const daysSinceEpoch = Math.ceil((date.getTime()) / MS_PER_DAY);
     const daysSinceExcelEpoch = daysSinceEpoch + EPOCH_OFFSET_DAYS;
     return daysSinceExcelEpoch;
