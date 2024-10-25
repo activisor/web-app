475b09db2dcac7dd2bfef93c8bb8605d57db5845
Art Wu <50754138+artqwu@users.noreply.github.com>
2024-10-25 15:11:45 -0400

    configured no-index pages

---
+++ src/app/(no-index)/layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'Activisor - Group Activity Management',
+    description: 'Activisor creates smart Google Sheets schedules for recurring recreational group and team meetings and then actively manages attendance.',
+    applicationName: 'Activisor',
+    robots: {
+        index: false,
+        follow: false,
+    }
+}
+
+export default function NoIndexLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <section>
+            {children}
+        </section>
+    )
+}
